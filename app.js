f5dcd0fcb7108ce68fd02af8bb56ab622a2e05d3
Tesh Patel <tesh.p@hotmail.co.uk>
2021-10-27 22:41:12 +0100

    Skeleton UI and Javascript built

---
+++ app.js
@@ -0,0 +1,55 @@
+/* grab elements from input*/
+
+/* declare new variables */
+let runningTotal = 0
+let resetValue = 0
+
+/* function for each calcualtor key
+- percentage conversion (convert %number into decimal eg 100% = 1)
+- add, subtract, multiply, divide, minus
+- sum total / running total
+- AC (reset)
+*/
+
+
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply() {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+};
+
+function minus(a, b) {
+    return a - b;
+};
+
+function percentageConversion(a) {
+    return a / 100;
+};
+
+
+function clear {
+    // xxx 
+};
+
+
+// Master function 
+function operate(val1, op, val2) {
+    // a = first value, op = operator key that is pressed eg add, minus etc, b = second value
+};
+
+
+/* display function */
+
+/* event listeners */
+
+/* logic */
