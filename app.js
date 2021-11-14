96cb2f9dbf0db3bb1edcf178b329d13c61563dc2
Tesh Patel <tesh.p@hotmail.co.uk>
2021-11-14 15:23:55 +0000

    To do: refactor If statement, round decimal

--- app.js
+++ app.js
@@ -1,85 +1,91 @@
-// * grab elements from html */
+// * grab elements from html 
 const displayBox = document.getElementById('display-result');
 const displayCalcBox = document.getElementById('display-calc');
 const numbBtn = document.querySelectorAll(".number-btns");
 const opBtn = document.querySelectorAll(".op-btns");
-// const opBtnValue = document.querySelectorAll(".op-btns").value;
-const valBtn = document.querySelectorAll(".val-btn");
-const allBtns = document.querySelectorAll(".btn");
 const resetBtn = document.getElementById("clear");
 const delBtn = document.getElementById("del");
-const sumKey = document.getElementById("equals")
-//const TBCKey = document.getElementById('X');
+const decimalBtn = document.getElementById("decimal");
+const sumKey = document.getElementById("equals");
 
+// Declare data variables
+let firstValue = "";
+let userOp = "";
+let secValue = "";
+let runningTotal;
 
-let userNo1 = [];
-let userOp = [];
-let userNo2 = [];
-
-//Event listeners to obtain input values and update display fields
-
+//Event listeners to obtain input values and update display fields on each click
 numbBtn.forEach((numberBtn) => {
+
+    decimalBtn.addEventListener("click", (e) => {
+        decimalBtn.disabled = true;
+    });
+
     numberBtn.addEventListener("click", (e) => {
-        // if userOp !== * or + or - etc ...then add value to userNo1...else add the value to userNo2
-        if (userOp == "") {
-            userNo1 += e.target.value;
-            displayCalcBox.innerHTML = userNo1;
-            console.log(userNo1);
+        if (userOp == "" || runningTotal == "") {
+            firstValue += e.target.value;
+            displayCalcBox.innerHTML = firstValue;
+            console.log(firstValue);
         } else {
-            userNo2 += e.target.value;
-            displayCalcBox.innerHTML = userNo1 + userOp + userNo2;
-            console.log(userNo1 + userOp + userNo2)
-            //oneRound(userNo1, userOp, userNo2);
+            secValue += e.target.value;
+            currentDisplay = runningTotal + userOp + secValue;
+            displayCalcBox.innerHTML = secValue;
+            console.log(firstValue + userOp + secValue)
         }
     })
 });
 
 opBtn.forEach(op => {
     op.addEventListener("click", (e) => {
-        userOp = e.target.value;
-        displayCalcBox.innerHTML = userNo1 + userOp + userNo2;
-        console.log(userOp)
+        userOp += e.target.value;
+        displayCalcBox.innerHTML = userOp;
+        return userOp
     })
 });
 
-// function to run calcualtor
-// sumKey.addEventListener("click", oneRound(userNo1, userOp, userNo2)) {};
+// Run full calcualtor by pressing equals key and display final result
+sumKey.addEventListener("click", function () {
+    decimalBtn.disabled = false;
+
+    if (userOp == "/" && secValue == "0") {
+        displayCalcBox.innerHTML = "Error - zero division"
+    } else {
+
+        if (userOp == "+") {
+            runningTotal = firstValue + secValue;
+            displayBox.innerHTML = runningTotal;
+            return runningTotal
+        } else if (userOp == "*") {
+            runningTotal = firstValue * secValue;
+            displayBox.innerHTML = runningTotal;
+            return runningTotal
+        } else if (userOp == "-") {
+            runningTotal = firstValue - secValue;
+            displayBox.innerHTML = runningTotal;
+            return runningTotal
+        } else if (userOp == "/") {
+            runningTotal = firstValue / secValue;
+            displayBox.innerHTML = runningTotal;
+            return runningTotal
+        } else if (userOp == "%") {
+            runningTotal = (firstValue / 100) * secValue;
+            displayBox.innerHTML = runningTotal;
+            return runningTotal;
+        }
+
+    }
 
 
-// Reset calculator
+
+    // rewrite above as switch statement
+});
+
+// Reset calculator and all variables
 resetBtn.addEventListener("click", function (e) {
-    userNo1 = "";
-    userNo2 = "";
+    firstValue = "";
+    secValue = "";
     userOp = "";
     displayCalcBox.innerHTML = 0;
     displayBox.innerHTML = 0;
+    decimalBtn.disabled = false;
 });
-
-
-//Operator function and display final total
-function oneRound(a, op, b) {
-    let runningTotal;
-    if (op == "+") {
-        runningTotal = a + b;
-        displayBox.innerHTML = runningTotal;
-        return runningTotal
-    } else if (op == "*") {
-        runningTotal = a * b;
-        displayBox.innerHTML = runningTotal;
-        return runningTotal
-    } else if (op == "-") {
-        runningTotal = a - b;
-        displayBox.innerHTML = runningTotal;
-        return runningTotal
-    } else if (op == "/") {
-        runningTotal = a / b;
-        displayBox.innerHTML = runningTotal;
-        return runningTotal
-    } else if (op == "/") {
-        runningTotal = a / 100;
-        displayBox.innerHTML = runningTotal;
-        return runningTotal;
-    }
-}
-
-
